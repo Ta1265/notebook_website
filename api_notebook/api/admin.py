8daa5bfc45ed6731cf0a12cdda2d7e6982605e52
Ta1265 <tayloranderson1265@gmail.com>
2020-06-22 11:31:43 -0400

    built

--- api_notebook/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Task, StickyBoard, StickyNote
-# Register your models here.
-
-admin.site.register(Task)
-admin.site.register(StickyNote)
-admin.site.register(StickyBoard)
